39751954ace10743441c55469f8a4822538489e5
Slattz <Slattz@users.noreply.github.com>
2020-03-03 23:17:03 +0000

    basic driver

--- WordSearchDriver.java
+++ WordSearchDriver.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class WordSearchDriver {
     public static void main(String[] args) {
-        System.out.println("Driver none");
+        String words = "animal crossing cowboard such amazing great game";
+        ArrayList<String> lol = new ArrayList<String>(Arrays.asList(words.split(" ")));
+        WordSearchPuzzle puz = new WordSearchPuzzle(lol);
+
+        puz.showWordSearchPuzzle(false);
+        System.out.println("\n\n" + puz.getPuzzleAsString());
     }
 }
